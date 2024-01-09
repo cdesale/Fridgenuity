c174a0f17bc54211802ce22c881d69d673a76ecd
Chaitali Desale <chaitali.desale92@gmail.com>
2024-01-09 10:54:08 +0000

    fixed the refresh issue for use context

--- components/UserContext.jsx
+++ components/UserContext.jsx
@@ -3,7 +3,20 @@ import React, { createContext, useState } from "react";
 export const UserContext = createContext();
 
 export const UserProvider = ({ children }) => {
-  const [user, setUser] = useState(null);
+  const getUserFromLocalStorage = () => {
+    return JSON.parse(localStorage.getItem("user")) || null;
+  };
+
+  const storeUserInLocalStorage = (userData) => {
+    localStorage.setItem("user", JSON.stringify(userData));
+  };
+
+  const [user, setUserState] = useState(getUserFromLocalStorage());
+
+  const setUser = (newUserData) => {
+    setUserState(newUserData);
+    storeUserInLocalStorage(newUserData);
+  };
 
   return (
     <UserContext.Provider value={{ user, setUser }}>
